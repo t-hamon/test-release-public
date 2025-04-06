6ea6f7bc45ca189f03607548016786c6ef42b081
Francois-Manuel Billault <fmbillault@users.noreply.github.com>
2025-04-06 13:18:58 +0200

    Create db.py

---
+++ src/api/db.py
@@ -0,0 +1,11 @@
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+def get_db_connection():
+    db_url = os.getenv("DATABASE_URL")
+    if not db_url:
+        raise RuntimeError("DATABASE_URL environment variable is not set")
+
+    conn = psycopg2.connect(db_url, cursor_factory=RealDictCursor)
+    return conn
