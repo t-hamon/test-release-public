679c628c250b1a3530c63b09245d82ce2659c9c3
Francois-Manuel Billault <fmbillault@users.noreply.github.com>
2025-04-06 13:22:20 +0200

    Update db.py

--- src/api/db.py
+++ src/api/db.py
@@ -3,7 +3,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 
 def get_db_connection():
-    db_url = os.getenv("DATABASE_URL")
+    db_url = os.getenv("DATABASE_URL")  # Read the DATABASE_URL from environment variables
     if not db_url:
         raise RuntimeError("DATABASE_URL environment variable is not set")
 
