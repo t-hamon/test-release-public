1c4509c6946c11e105cc003beab3225452671083
Francois-Manuel Billault <fmbillault@users.noreply.github.com>
2025-04-06 13:14:46 +0200

    Create init.sql

---
+++ data/init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE movies (
+    MovieName TEXT,
+    MovieID INT,
+    Genre TEXT,
+    UserID INT,
+    Rating INT,
+    Timestamp BIGINT,
+    Gender CHAR(1),
+    Age INT,
+    Occupation INT,
+    Zipcode TEXT,
+    age_group TEXT
+);
+
+COPY movies(MovieName,MovieID,Genre,UserID,Rating,Timestamp,Gender,Age,Occupation,Zipcode,age_group)
+FROM '/docker-entrypoint-initdb.d/movies_cleaned.csv'
+DELIMITER ','
+CSV HEADER;
